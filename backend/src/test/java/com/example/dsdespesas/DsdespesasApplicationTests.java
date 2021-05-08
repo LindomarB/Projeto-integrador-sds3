9eb18b72fe3d2c1e5e82ad628779715aad761549
LindomarB <lindomar1802@gmail.com>
2021-05-07 22:52:10 -0300

    Projetos Criados

---
+++ backend/src/test/java/com/example/dsdespesas/DsdespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsdespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
